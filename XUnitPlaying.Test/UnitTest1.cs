944f2c0b03469ec9baf3b4af65d461570267c212
sa-es-ir <s.esmaeelinejad@gmail.com>
2023-08-02 22:24:16 +0700

    add first test

--- XUnitPlaying.Test/UnitTest1.cs
+++ XUnitPlaying.Test/UnitTest1.cs
@@ -1,11 +1,29 @@
-namespace XUnitPlaying.Test
-{
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
+using System.Diagnostics;
+using System.Reflection;
+using Xunit;
+using Xunit.Sdk;
 
-        }
+namespace XUnitPlaying.Test;
+
+public class BeforeAfterAttribute : BeforeAfterTestAttribute
+{
+    public override void Before(MethodInfo methodUnderTest)
+    {
+        Debug.WriteLine($"Before Method: {methodUnderTest.Name}");
+    }
+
+    public override void After(MethodInfo methodUnderTest)
+    {
+        Debug.WriteLine($"After Method: {methodUnderTest.Name}");
+    }
+}
+
+public class UnitTest1
+{
+    [Fact]
+    [BeforeAfter]
+    public void Test1()
+    {
+
     }
 }
