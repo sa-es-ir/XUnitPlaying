c8d7a42a1f475b0031bd9d3920f796518c4df8a6
sa-es-ir <s.esmaeelinejad@gmail.com>
2023-08-02 22:25:07 +0700

    add before after attribute

---
+++ XUnitPlaying.Test/BeforeAfterTest.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using Xunit.Sdk;
+
+namespace XUnitPlaying.Test;
+
+//[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
