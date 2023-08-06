e1f688302a9974150790aea91cf100b06473e5fd
sa-es-ir <s.esmaeelinejad@gmail.com>
2023-08-06 12:14:59 +0700

    update the beforeafter attribute class

--- XUnitPlaying.Test/BeforeAfterTest.cs
+++ XUnitPlaying.Test/BeforeAfterTest.cs
@@ -5,4 +5,16 @@ using Xunit.Sdk;
 
 namespace XUnitPlaying.Test;
 
-//[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+public class BeforeAfterAttribute : BeforeAfterTestAttribute
+{
+    public override void Before(MethodInfo methodUnderTest)
+    {
+        Debug.WriteLine($"Before Method: {methodUnderTest.Name}");
+    }
+
+    public override void After(MethodInfo methodUnderTest)
+    {
+        Debug.WriteLine($"After Method: {methodUnderTest.Name}");
+    }
+}
