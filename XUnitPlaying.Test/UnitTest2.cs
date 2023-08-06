496c770ae591211c5b112df0f219f1ec8c2136ec
sa-es-ir <s.esmaeelinejad@gmail.com>
2023-08-06 12:20:40 +0700

    add new test clss

---
+++ XUnitPlaying.Test/UnitTest2.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace XUnitPlaying.Test;
+
+//[Collection("test")]
+public class UnitTest2
+{
+    [Fact]
+    [BeforeAfter]
+    public async Task Test2()
+    {
+        Assert.True("test".Equals("test"));
+        await Task.Delay(3000);
+    }
+}
